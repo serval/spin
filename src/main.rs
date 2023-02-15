1cf66278f7d3f6e1e3dfb28eaf833f0e15664c52
Mark Christian <m@rkchristian.ca>
2023-02-15 15:17:54 -0800

    Prefix stderr as well

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
-use std::io::Write;
-use std::process::{exit, Child, ChildStdout, Command, Stdio};
-
 use nonblock::NonBlockingReader;
+use std::io::{Read, Write};
+use std::os::fd::AsRawFd;
+use std::process::{exit, Child, ChildStderr, ChildStdout, Command, Stdio};
+
+fn emit_prefixed_output(pid: u32, nonblock_stdio: &mut NonBlockingReader<impl AsRawFd + Read>) {
+    let mut buf = vec![];
+    let bytes_read = nonblock_stdio
+        .read_available(&mut buf)
+        .expect("Failed to read from child process");
+
+    if bytes_read > 0 {
+        match String::from_utf8(buf) {
+            Ok(str) => {
+                // prefix every line of output with the child's process number
+                let prefixed_str = str
+                    .trim()
+                    .split("\n")
+                    .map(|line| format!("[{pid}] {line}"))
+                    .collect::<Vec<String>>()
+                    .join("\n");
+                println!("{prefixed_str}");
+            }
+            Err(err) => {
+                // child isn't writing UTF-8 data, apparently; just write it out as-is
+                // (it's possible that we just read in the middle of a multi-byte sequence, but
+                // there's no way to know)
+                let buf = err.into_bytes();
+                std::io::stdout()
+                    .write(&buf[..])
+                    .expect("Failed to write output");
+            }
+        };
+    }
+}
 
 fn main() {
     let mut args: Vec<_> = std::env::args_os().skip(1).collect();
@@ -17,12 +48,17 @@ fn main() {
     let cmd = args.remove(0);
 
     // Spin up N copies of the command
-    let mut children: Vec<(Child, NonBlockingReader<ChildStdout>)> = vec![];
+    let mut children: Vec<(
+        Child,
+        NonBlockingReader<ChildStdout>,
+        NonBlockingReader<ChildStderr>,
+    )> = vec![];
     let mut cmd = Command::new(cmd);
     cmd.args(&args);
     cmd.stdin(Stdio::null());
     for _ in 0..num_copies {
         cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
         let mut handle = match cmd.spawn() {
             Ok(handle) => handle,
             Err(err) => panic!("Failed to spawn: {err:?}"),
@@ -30,11 +66,17 @@ fn main() {
         let Some(stdout) = handle.stdout.take() else {
             panic!("Failed to get stdout from child");
         };
+        let Some(stderr) = handle.stderr.take() else {
+            panic!("Failed to get stderr from child");
+        };
         let Ok(nonblock_stdout) = NonBlockingReader::from_fd(stdout) else {
             panic!("Failed to make non-blocking reader for stdout");
         };
+        let Ok(nonblock_stderr) = NonBlockingReader::from_fd(stderr) else {
+            panic!("Failed to make non-blocking reader for stderr");
+        };
 
-        children.push((handle, nonblock_stdout));
+        children.push((handle, nonblock_stdout, nonblock_stderr));
     }
 
     // Children are removed from this list when they stop producing output; as long as we have
@@ -44,38 +86,12 @@ fn main() {
         // order by index so we can remove children that have finished without disturbing the other
         // children we haven't iterated over yet.
         let mut remaining_children = vec![];
-        for (handle, mut nonblock_stdout) in children.into_iter() {
-            let mut buf = vec![];
-            let bytes_read = nonblock_stdout
-                .read_available(&mut buf)
-                .expect("Failed to read from child process");
-
-            if bytes_read > 0 {
-                match String::from_utf8(buf) {
-                    Ok(str) => {
-                        // prefix every line of output with the child's process number
-                        let prefixed_str = str
-                            .trim()
-                            .split("\n")
-                            .map(|line| format!("[{}] {}", handle.id(), line))
-                            .collect::<Vec<String>>()
-                            .join("\n");
-                        println!("{prefixed_str}");
-                    }
-                    Err(err) => {
-                        // child isn't writing UTF-8 data, apparently; just write it out as-is
-                        // (it's possible that we just read in the middle of a multi-byte sequence, but
-                        // there's no way to know)
-                        let buf = err.into_bytes();
-                        std::io::stdout()
-                            .write(&buf[..])
-                            .expect("Failed to write output");
-                    }
-                };
-            }
+        for (handle, mut nonblock_stdout, mut nonblock_stderr) in children.into_iter() {
+            emit_prefixed_output(handle.id(), &mut nonblock_stdout);
+            emit_prefixed_output(handle.id(), &mut nonblock_stderr);
 
             if !nonblock_stdout.is_eof() {
-                remaining_children.push((handle, nonblock_stdout));
+                remaining_children.push((handle, nonblock_stdout, nonblock_stderr));
             }
         }
         children = remaining_children;
